d8d552c33f4985d309cc276f93a6bf570a47fbdd
jenh <jenh@ab32534a-6a03-4b03-b9b7-2caa0d460b98>
2011-01-08 06:53:09 +0000

    Added the resolve method to Locator and brought the quoting more into line with the tests.

--- tmapi/models/locator.py
+++ tmapi/models/locator.py
@@ -1,5 +1,6 @@
 import unicodedata
-from urllib import quote, unquote
+import urllib
+import urlparse
 
 
 class LocatorBase (object):
@@ -18,6 +19,19 @@ class LocatorBase (object):
         """
         return self._reference
 
+    def resolve (self, reference):
+        """Resolves the `reference` against this locator.
+
+        The returned `Locator` represents an absolute IRI.
+
+        :param reference: the reference which should be resolved
+          against this locator
+        :type reference: string
+        :rtype: `Locator`
+
+        """
+        return Locator(urlparse.urljoin(self._external, reference))
+    
     def to_external_form (self):
         """Returns the external form of the IRI.
 
@@ -30,14 +44,16 @@ class LocatorBase (object):
         return self._external
 
     def normalise (self, reference):
-        reference = unicode(quote(reference), 'utf-8', 'replace')
-        return reference
+        parts = list(urlparse.urlsplit(reference))
+        parts[2] = urllib.quote(parts[2], '/;')
+        url = unicode(urlparse.urlunsplit(parts), 'utf-8', 'replace')
+        return url
     
     def unnormalise (self, reference):
         if not isinstance(reference, unicode):
-            reference = unicode(unquote(reference), 'utf-8', 'replace')
+            reference = unicode(urllib.unquote(reference), 'utf-8', 'replace')
         else:
-            reference = unquote(reference)
+            reference = urllib.unquote(reference)
         return unicodedata.normalize('NFC', reference).encode('utf-8')
 
     def __eq__ (self, other):
@@ -54,3 +70,5 @@ class Locator (LocatorBase):
     def __init__ (self, reference):
         self.generate_forms(reference)
 
+    def __unicode__ (self):
+        return self._reference
