49cef67e4c8808bdcbc10e420d44873d4d443955
jenh <jenh@ab32534a-6a03-4b03-b9b7-2caa0d460b98>
2011-01-08 06:51:43 +0000

    Added a module containing constants.

---
+++ tmapi/constants.py
@@ -0,0 +1,7 @@
+# Datatype URIs.
+XSD = 'http://www.w3.org/2001/XMLSchema#'
+XSD_ANY_URI = XSD + 'anyURI'
+XSD_FLOAT = XSD + 'float'
+XSD_INT = XSD + 'int'
+XSD_LONG = XSD + 'long'
+XSD_STRING = XSD + 'string'
