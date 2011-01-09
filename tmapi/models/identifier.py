38aec3a016ad9c907cd685e106f132d1057759b4
jenh <jenh@ab32534a-6a03-4b03-b9b7-2caa0d460b98>
2011-01-09 08:10:38 +0000

    Actually added the Identifier model.

---
+++ tmapi/models/identifier.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Identifier (models.Model):
+
+    #identifier = models.CharField(max_length=24)
+    # containing_topic_map may be null because when a TopicMap object
+    # is first created (before it is saved) it has no database ID.
+    containing_topic_map = models.ForeignKey(
+        'TopicMap', related_name='identifiers_in_map', null=True)
+
+    class Meta:
+        app_label = 'tmapi'
+
+    def get_construct (self):
+        """Returns the `Construct` that this is an identifier for.
+
+        :rtype: `Construct` or None
+
+        """
+        construct = None
+        construct_types = ('association', 'name', 'occurrence', 'role',
+                           'topic', 'topicmap', 'variant')
+        for construct_type in construct_types:
+            try:
+                construct = getattr(self, construct_type)
+            except:
+                pass
+        return construct
+
+        
