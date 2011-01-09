cc7fa04393b85c03ef8979e2b4b9232574349355
jenh <jenh@ab32534a-6a03-4b03-b9b7-2caa0d460b98>
2011-01-09 01:27:52 +0000

    Started work on methods for datatype aware constructs.

--- tmapi/models/datatype_aware.py
+++ tmapi/models/datatype_aware.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from tmapi.constants import XSD_ANY_URI, XSD_FLOAT, XSD_INT, XSD_LONG, \
+    XSD_STRING
+
 from locator import Locator
+from reifiable import Reifiable
 from scoped import Scoped
 
 
-class DatatypeAware (Scoped):
+class DatatypeAware (Reifiable, Scoped):
 
     """Common base interface for `Occurrence`s and `Variant`s."""
     
@@ -27,6 +31,45 @@ class DatatypeAware (Scoped):
         """Returns the lexical representation of the value."""
         return self.value
 
-    def set_value (self, value):
-        """Sets the string value."""
-        raise Exception('Not yet implemented')
+    def locator_value (self):
+        """Returns the `Locator` representation of the value.
+
+        :rtype: `Locator`
+
+        """
+        if self.datatype == XSD_ANY_URI:
+            return Locator(self.value)
+        raise TypeError('Value is not a Locator')
+    
+    def set_value (self, value, datatype=None):
+        """Sets the value.
+
+        If `datatype` is None, the datatype will be implicitly set to
+        match the type of `value`.
+
+        :param value: the value
+        :param datatype: optional datatype of `value`
+        :type datatype: `Locator`
+
+        """
+        if datatype is None:
+            # This mapping is not comprehensive, since the mapping
+            # (from section 1.10.3 at
+            # http://cvs.zope.org/~checkout~/Packages/WebService/doc/WebService.html)
+            # has the same Python type mapped to multiple XSD types.
+            if isinstance(value, str):
+                datatype = XSD_STRING
+            elif isinstance(value, Locator):
+                datatype = XSD_ANY_URI
+                value = value.to_external_form()
+            elif isinstance(value, float):
+                datatype = XSD_FLOAT
+            elif isinstance(value, int):
+                datatype = XSD_INT
+            elif isinstance(value, long):
+                datatype = XSD_LONG
+        else:
+            datatype = datatype.to_external_form()
+        self.value = value
+        self.datatype = datatype
+        self.save()
