3ec7376e3984afdb7c530f309bc529d50182de56
jenh <jenh@ab32534a-6a03-4b03-b9b7-2caa0d460b98>
2011-01-09 00:11:50 +0000

    Added construct tests of other construct types.

--- tmapi/tests/models/construct_tests.py
+++ tmapi/tests/models/construct_tests.py
@@ -38,8 +38,42 @@ class ConstructTest (TestCase):
         id = construct.get_id()
         self.assertEqual(construct, tm.get_construct_by_id(id),
                          'Unexpected result')
-        
 
     def test_topic_map (self):
         """Construct tests against a topic map."""
         self._test_construct(self.tm)
+
+    def test_topic (self):
+        """Construct tests against a topic."""
+        topic = self.tm.create_topic_by_subject_locator(
+            self.tm.create_locator('http://www.tmapi.org/'))
+        self._test_construct(topic)
+
+    def test_association (self):
+        """Construct tests against an association."""
+        association = self.tm.create_association(self.tm.create_topic())
+        self._test_construct(association)
+
+    def test_role (self):
+        """Construct tests against a role."""
+        association = self.tm.create_association(self.tm.create_topic())
+        role = association.create_role(self.tm.create_topic(),
+                                       self.tm.create_topic())
+        self._test_construct(role)
+
+    def test_occurrence (self):
+        """Construct tests against an occurrence."""
+        occurrence = self.tm.create_topic().create_occurrence(
+            self.tm.create_topic(), 'Occurrence')
+        self._test_construct(occurrence)
+
+    def test_name (self):
+        """Construct tests against a name."""
+        name = self.tm.create_topic().create_name('Name')
+        self._test_construct(name)
+
+    def test_variant (self):
+        """Construct tests against a variant."""
+        name = self.tm.create_topic().create_name('Name')
+        variant = name.create_variant('Variant', [])
+        self._test_construct(variant)
