b6f315944dcc40fcce73bda3e301563269cecb48
Jamie Norrish <jamie@artefact.org.nz>
2015-03-05 12:25:48 +1300

    Added index on address field in SubjectIdentifier for big performance win.

--- tmapi/models/subject_identifier.py
+++ tmapi/models/subject_identifier.py
@@ -20,7 +20,7 @@ from locator import LocatorBase
 class SubjectIdentifier (LocatorBase, models.Model):
 
     topic = models.ForeignKey('Topic', related_name='subject_identifiers')
-    address = models.CharField(max_length=512)
+    address = models.CharField(db_index=True, max_length=512)
     containing_topic_map = models.ForeignKey(
         'TopicMap', related_name='subject_identifiers_in_map')
 
@@ -30,10 +30,10 @@ class SubjectIdentifier (LocatorBase, models.Model):
     def __init__ (self, *args, **kwargs):
         super(SubjectIdentifier, self).__init__(*args, **kwargs)
         self.generate_forms(self.address)
-        
+
     def save (self, *args, **kwargs):
         super(SubjectIdentifier, self).save(*args, **kwargs)
         self.generate_forms(self.address)
-        
+
     def __unicode__ (self):
         return self.address
