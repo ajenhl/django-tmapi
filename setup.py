b7df15e5f363bf2be0003e4c48ad1ab0db91713d
Jamie Norrish <jamie@artefact.org.nz>
2016-08-02 12:48:12 +1200

    Updated changelog and version number for release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'django_tmapi',
-    version = '1.0.0',
+    version = '1.1.0',
     description = 'Django app providing an implementation of the Topic Maps API 2.0',
     url = 'https://github.com/ajenhl/django-tmapi',
     author = 'Jamie Norrish',
